5b4d1e7ce48d9964f8ef799c45f9a31400707fcd
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-12-15 14:43:05 +0000

    Added two new directories data_types and variables to the repository

---
+++ basics/data_types/DataTypes.java
@@ -0,0 +1,19 @@
+class DataTypes {
+    public static void main(String args[]) {
+        // Integer data types
+        byte b = 10;
+        short s = 20;
+        int i = 30;
+        long l = 40l;
+
+        // Floating point data types
+        float f = 50.0f;
+        double d = 60.0;
+
+        // Character data type
+        char c = 'a';
+
+        // Boolean data type
+        boolean bool = true;
+    }
+}
---
+++ basics/variables/Variables.java
@@ -0,0 +1,8 @@
+class Variables {
+    public static void main(String args[]) {
+        int x = 10;
+        int y = 20;
+        int sum = x + y;
+        System.out.println("The sum of " + x + " and " + y + " is " + sum);
+    }
+}
