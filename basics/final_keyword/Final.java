3daaf96d384297d771cd23afe422ff43eee964ce
Desmond Asiedu Asamoah <walterobrien_123@yahoo.com>
2023-12-27 17:14:57 +0000

    Added new directory final_keyword.

--- basics/final_keyword/Final.java
+++ basics/final_keyword/Final.java
@@ -1,7 +1,7 @@
-package final_keyword;
-
 public class Final {
     public static void main(String args[]) {
-        
+        final String model = "Mustang";
+        model = "Ferrari"; // Compile time error: "cannot assign a value to final variable model
+        System.out.println(model);
     }
 }
